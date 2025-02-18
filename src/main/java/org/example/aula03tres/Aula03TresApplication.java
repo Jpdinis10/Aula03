14ac73977446db0ff5041e236c1de1ad81f40422
aluno cmc <alunocmc@umc.br>
2025-02-18 10:44:36 -0300

    aula03

---
+++ src/main/java/org/example/aula03tres/Aula03TresApplication.java
@@ -0,0 +1,14 @@
+package org.example.aula03tres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula03TresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula03TresApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/org/example/aula03tres/Aula03TresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aula03tres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula03TresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
